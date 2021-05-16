18657aeca9cd071c5bcbeb43bafbca49747f5f2f
shreya shahi <67728684+sh-shrey@users.noreply.github.com>
2021-05-16 22:46:30 +0530

    Add files via upload

---
+++ Quiz/main.js
@@ -0,0 +1,356 @@
+
+function check(){
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
+	var question7 = document.quiz.question7.value;
+	var question8 = document.quiz.question8.value;
+	var question9 = document.quiz.question9.value;
+	var question10 = document.quiz.question10.value;
+	var question11 = document.quiz.question11.value;
+	var question12 = document.quiz.question12.value;
+	var question13 = document.quiz.question13.value;
+	var question14 = document.quiz.question14.value;
+	var question15 = document.quiz.question15.value;
+	var question16 = document.quiz.question16.value;
+	var question17 = document.quiz.question17.value;
+	var question18 = document.quiz.question18.value;
+	var question19 = document.quiz.question19.value;
+	var question20 = document.quiz.question20.value;
+	var question21 = document.quiz.question21.value;
+	var question22 = document.quiz.question22.value;
+	var question23 = document.quiz.question23.value;
+	var question24 = document.quiz.question24.value;
+	var question25 = document.quiz.question25.value;
+	var question26 = document.quiz.question26.value;
+	var question27 = document.quiz.question27.value;
+	var question28 = document.quiz.question28.value;
+	var question29 = document.quiz.question29.value;
+	var question30 = document.quiz.question30.value;
+	var question31 = document.quiz.question31.value;
+	var question32 = document.quiz.question32.value;
+	var question33 = document.quiz.question33.value;
+	var question34 = document.quiz.question34.value;
+	var question35 = document.quiz.question35.value;
+	var question36 = document.quiz.question36.value;
+	var question37 = document.quiz.question37.value;
+	var question38 = document.quiz.question38.value;
+	var question39 = document.quiz.question39.value;
+	var question40 = document.quiz.question40.value;
+	var question41 = document.quiz.question41.value;
+	var question42 = document.quiz.question42.value;
+	var question43 = document.quiz.question43.value;
+	var question44 = document.quiz.question44.value;
+	var question45 = document.quiz.question45.value;
+	var question46 = document.quiz.question46.value;
+	var question47 = document.quiz.question47.value;
+	var question48 = document.quiz.question48.value;
+	var question49 = document.quiz.question49.value;
+	var question50 = document.quiz.question50.value;
+	var question51 = document.quiz.question51.value;
+	var question52 = document.quiz.question52.value;
+	var question53 = document.quiz.question53.value;
+	var question54 = document.quiz.question54.value;
+	var question55 = document.quiz.question55.value;
+	var question56 = document.quiz.question56.value;
+	var question57 = document.quiz.question57.value;
+	var question58 = document.quiz.question58.value;
+	var question59 = document.quiz.question59.value;
+	var question60 = document.quiz.question60.value;
+	var question61 = document.quiz.question61.value;
+	var question62 = document.quiz.question62.value;
+	var question63 = document.quiz.question63.value;
+	var question64 = document.quiz.question64.value;
+	var question65 = document.quiz.question65.value;
+	var question66 = document.quiz.question66.value;
+	var question67 = document.quiz.question67.value;
+	var question68 = document.quiz.question68.value;
+	var question69 = document.quiz.question69.value;
+	var question70 = document.quiz.question70.value;
+	var question71 = document.quiz.question71.value;
+	var question72 = document.quiz.question72.value;
+	var question73 = document.quiz.question73.value;
+	var question74 = document.quiz.question74.value;
+	var question75 = document.quiz.question75.value;
+	
+	var correct = 0;
+	var total_questions =75; //total no.of questions in quiz
+
+	
+
+    if (question1 == "G.J. Mendel") {
+		correct++;
+	}
+    if (question2 == "Chittaranjan Das") {
+		correct++;
+    }
+    if (question3 == "English") {
+		correct++;
+	}
+	if (question4 == "Dehra Dun") {
+		correct++;
+	}
+	if (question5 == "Jayadev") {
+		correct++;
+	}
+	if (question6 == "13") {
+		correct++;
+	}
+	if (question7 == "1995") {
+		correct++;
+	}
+	if (question8 == "Bureaucracy") {
+		correct++;
+	}
+	if (question9 == "1946") {
+		correct++;
+	}
+	if (question10 == "State list") {
+		correct++;
+	}
+	if (question11 == "44 amendment") {
+		correct++;
+	}
+	if (question12 == "Bana Bhatta") {
+		correct++;
+	}
+	if (question13 == "1776") {
+		correct++;
+	}
+	if (question14 == "Adolf Hitler") {
+		correct++;
+	}
+	if (question15 == "Pacific") {
+		correct++;
+	}
+	if (question16 == "an Indian astronomer and famous mathematician") {
+		correct++;
+	}
+	if (question17 == "65.38%") {
+		correct++;
+	}
+	if (question18 == "1985") {
+		correct++;
+	}
+	if (question19 == "Bihar") {
+		correct++;
+	}
+	if (question20 == "Norway") {
+		correct++;
+	}
+	if (question21 == "Jakarta") {
+		correct++;
+	}
+	if (question22 == "Cauvery") {
+		correct++;
+	}
+	if (question23 == "Prashant Sawant") {
+		correct++;
+	}
+	if (question24 == "At the border of the Indian state of Goa and Karnataka") {
+		correct++;
+	}
+	if (question25 == "Gaurav Bidhuri") {
+		correct++;
+	}
+	if (question26 == "5400 Metre") {
+		correct++;
+	}
+	if (question27 == "Snehlata Shrivastava") {
+		correct++;
+	}
+	if (question28 == "Island") {
+		correct++;
+	}
+	if (question29 == "Indonesia") {
+		correct++;
+	}
+	if (question30 == "Arabia") {
+		correct++;
+	}
+	if (question31 == "Mana Pass") {
+		correct++;
+	}
+	if (question32 == "Tropic of Cancer") {
+		correct++;
+	}
+	if (question33 == "Arabian Sea") {
+		correct++;
+	}
+	if (question34 == "manipur") {
+		correct++;
+	}
+	if (question35 == "The Ganga") {
+		correct++;
+	}
+
+	if (question36 == "ivory") {
+		correct++;
+    }
+	if (question37 == "25 January 2017") {
+		correct++;
+    }
+    if (question38 == "1792 In Calcutta") {
+		correct++;
+}
+
+if (question39 == "1937") {
+		correct++;
+}
+
+if (question40 == "Pema Khandu") {
+		correct++;
+}
+
+if (question41 == "SBI") {
+		correct++;
+}
+
+if (question42 == "G.V. Mavalankar") {
+		correct++;
+}
+
+if (question43 == "118") {
+		correct++;
+}
+
+if (question44 == "Adam Smith") {
+		correct++;
+}
+
+if (question45 == "Italy") {
+		correct++;
+}
+
+if (question46 == "Karnataka") {
+		correct++;
+}
+
+if (question47 == "Mumbai") {
+		correct++;
+}
+
+if (question48 == "1950") {
+		correct++;
+}
+
+if (question49 == "Appa Sherpa") {
+		correct++;
+}
+
+if (question50 == "Arunachal Pradesh") {
+		correct++;
+}
+
+if (question51 == "Imraan Khan") {
+		correct++;
+}
+
+if (question52 == "Inter-Services Intelligence") {
+		correct++;
+}
+
+if (question53 == "April 22") {
+		correct++;
+}
+
+if (question54 == "Tripura") {
+		correct++;
+}
+
+if (question55 == "2011") {
+		correct++;
+}
+
+if (question56 == "2016") {
+		correct++;
+}
+
+if (question57 == "Ministry of Skill Development and Entrepreneurship") {
+		correct++;
+}
+
+if (question58 == "Shri Venkaiah Naidu ") {
+		correct++;
+}
+
+if (question59 == "Airtel") {
+		correct++;
+}
+
+if (question60 == "Surveyor General of India") {
+		correct++;
+}
+
+if (question61 == "Cuba") {
+		correct++;
+}
+
+if (question62 == "Prime Minister of India") {
+		correct++;
+}
+
+if (question63 == "European unification") {
+		correct++;
+}
+
+if (question64 == "February 28") {
+		correct++;
+}
+
+if (question65 == "Dili") {
+		correct++;
+}
+
+if (question66 == "Sucheta Kripalani") {
+		correct++;
+}
+
+if (question67 == "7 December 2016 ") {
+		correct++;
+}
+
+if (question68 == "Sahara Desert") {
+		correct++;
+}
+
+if (question69 == "Chamoli District") {
+		correct++;
+}
+
+if (question70 == "Taipei") {
+		correct++;
+}
+
+if (question71 == "Seoul") {
+		correct++;
+}
+
+if (question72 == "Singapore") {
+		correct++;
+}
+
+if (question73 == "3000") {
+		correct++;
+}
+
+if (question74 == "Jeff Bezos") {
+		correct++;
+}
+
+if (question75 == "14th April") {
+		correct++;
+}	
+    var not_correct = correct - total_questions;
+    var not_correct = correct - total_questions;
+	var negative = Math.abs(not_correct);
+	var score = correct*4 - negative;
+	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("number_correct").innerHTML = "Score: " + score + "/300" ;
+	}
+	
+	
+	
